5ee1f6eb5eca9851f5a3531e6d17acd7a57cddfc
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2021-11-27 11:38:49 -0500

    Update fileSort.py

--- fileSort.py
+++ fileSort.py
@@ -24,12 +24,29 @@ def formatData(loc, sizeTreshold):
 						data[date] = [(lon, lat)]
 					else:
 						data[date].append((lon, lat))
-	return pd.DataFrame.from_dict(data, orient = 'index')
+	data = pd.DataFrame.from_dict(data, orient = 'index')
+	data.ffill(inplace = True)
+	return data
 
 data = formatData(os.getcwd() + '\\taxi_log_2008_by_id', 10000)
 
-print(data)
+#interactions dataframe
+
+#flip dataset
+dfTime = data.transpose()
+for index, row in dfTime.iterrows():
+	#compare each value in a column to eachother and
+	for i in dfTime[index]:
+	#if they match replace the match values with the macher, else replace with 0
+		if dfTime.iloc[index][i] ==
+	#do for each column
+	#unfilp the dataframe and return it
+
+def slowFunc(df):
+	for column in df:
+		a
 
 with open('COVIDdata2.pkl', 'wb') as f:
 	pickle.dump(data, f)
 	f.close()
+
