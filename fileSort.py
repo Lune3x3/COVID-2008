931dc8c41fa9d449fbd95ac915073c1f6960ad73
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2021-11-27 18:16:03 -0500

    Update fileSort.py

--- fileSort.py
+++ fileSort.py
@@ -1,6 +1,7 @@
 import os, time, pickle
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 def formatData(loc, sizeTreshold):
 	files = os.listdir(loc)
@@ -30,23 +31,6 @@ def formatData(loc, sizeTreshold):
 
 data = formatData(os.getcwd() + '\\taxi_log_2008_by_id', 10000)
 
-#interactions dataframe
-
-#flip dataset
-dfTime = data.transpose()
-for index, row in dfTime.iterrows():
-	#compare each value in a column to eachother and
-	for i in dfTime[index]:
-	#if they match replace the match values with the macher, else replace with 0
-		if dfTime.iloc[index][i] ==
-	#do for each column
-	#unfilp the dataframe and return it
-
-def slowFunc(df):
-	for column in df:
-		a
-
 with open('COVIDdata2.pkl', 'wb') as f:
 	pickle.dump(data, f)
 	f.close()
-
