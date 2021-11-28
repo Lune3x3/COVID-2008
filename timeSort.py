cf622c4bed7da7959585c094e5185bc2e8a8db7f
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2021-11-27 19:58:31 -0500

    Update timeSort.py

--- timeSort.py
+++ timeSort.py
@@ -1,32 +1,52 @@
 import pickle
 import pandas as pd
 import numpy as np
+from time import time
 from collections import defaultdict
 
 with open('COVIDdata2.pkl', 'rb') as f:
 	df = pickle.load(f).sort_index(axis = 0)
 	f.close()
 
-dfTime = pd.DataFrame(index=list(df.index))
+#dfTime = pd.DataFrame(index=list(df.index))
+#start = time()
+#for label, series in df.T.iteritems():
+#	idx = []
+#	#hMap = defaultdict(bool)
+#	s = series.to_numpy()
+#
+#	for i in range(len(series)):
+#		#print(series[i])
+#
+#		matches = np.where(series == series[i])
+#		idx.append(matches)
+#		#print(idx)
+#
+#		#print(idx)
+#		#if hMap[series[i]] != 0:
+#			#idx.append(hMap[series[i]])
+#		#else:
+#			#hMap[series[i]] = i
+#	#print(label)
+#	break
+#end = time()
+#
+#print(end-start)
 
+dic = {}
 for label, series in df.T.iteritems():
+	print(label)
+	hMap = defaultdict(bool)
 	idx = []
-	#hMap = defaultdict(bool)
-	s = series.to_numpy()
-
 	for i in range(len(series)):
-		#print(series[i])
+		if hMap[series[i]] != 0:
+			idx.append((hMap[series[i]]))
+		else:
+			hMap[series[i]] = i
+			idx.append(False)
+	dic[label] = idx
+dfTime = pd.DataFrame.from_dict(dic, orient = 'index').sort_index(axis = 0)
 
-		matches = np.where(series == series[i])
-		idx.append(matches)
-		print(idx)
-		break
-	break
-		#print(idx)
-		#if hMap[series[i]] != 0:
-			#idx.append(hMap[series[i]])
-		#else:
-			#hMap[series[i]] = i
-	print(idx)
-
-#print(df[3520])
+with open('allInteractions.pkl', 'wb') as f:
+	pickle.dump(dfTime, f)
+	f.close()
